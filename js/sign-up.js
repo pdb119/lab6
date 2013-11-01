d5ba1f8c1e6942ca329c310d68590087c7c2fea9
unknown <iguest@IS-M334L23-29.ischool.uw.edu>
2013-11-01 12:17:51 -0700

    not done yet

--- js/sign-up.js
+++ js/sign-up.js
@@ -1,3 +1,26 @@
 //signup.js 
 // add your JavaScript code to this file
+window.onload = function() {
+	var stateSel = document.getElementById("state");
+	//var stateSel = $(".state");
+	var state;
+	for(state in usStates) {
+		var newState = document.createElement("option");
+		newState.setAttribute("value", usStates[state]["abbreviation"]);
+		newState.innerHTML = usStates[state]["name"];
+		stateSel.add(newState);
+	}
+	$(".signup-form").submit(submitted);
+};
+
+function submitted(){
+	var signupForm = $(this);
+	p(signupForm.find("input[name='addr-1']").val());
+	//p(signupform.firstname.value);
+	return false;
+}
+
+function p(stringMsg){
+	console.log(stringMsg);
+}
 
