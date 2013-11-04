542d9808fadce64b456509a852fc7bd04965dad5
pdb119 <bradypat@uw.edu>
2013-11-03 21:39:42 -0800

    finished

--- js/sign-up.js
+++ js/sign-up.js
@@ -2,7 +2,6 @@
 // add your JavaScript code to this file
 window.onload = function() {
 	var stateSel = document.getElementById("state");
-	//var stateSel = $(".state");
 	var state;
 	for(state in usStates) {
 		var newState = document.createElement("option");
@@ -11,16 +10,36 @@ window.onload = function() {
 		stateSel.add(newState);
 	}
 	$(".signup-form").submit(submitted);
+	$("#refer").change(referChanged);
+	$(".cancel-signup").click(function(){
+			$(".cancel-signup-modal").modal();							   
+		});
+	$(".btn-abandon").click(function(){
+			window.location("http://google.com");
+		});
+		
 };
 
 function submitted(){
 	var signupForm = $(this);
-	p(signupForm.find("input[name='addr-1']").val());
-	//p(signupform.firstname.value);
-	return false;
+	if(signupForm.find("input[name='addr-1']").val()){
+		if(!signupForm.find("input[name='zip']").val()){
+			alert("You must fill in the zip code to complete your address.");
+			return false;
+		}
+	}
+	return true;
 }
 
-function p(stringMsg){
-	console.log(stringMsg);
+function referChanged(){
+	var other = document.querySelector("input[name='refer-other']");
+	if(this.value == "other"){
+		other.style.visibility = "visible";
+		other.disabled = false;
+		other.focus();
+	} else {
+		other.style.visibility = "hidden";
+		other.disabled = true;
+	}
 }
 
